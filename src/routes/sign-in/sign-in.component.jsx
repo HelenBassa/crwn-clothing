3929a34ef27ef0a8563b943d233a747136e64e13
HelenBassa <helenbassa@gmail.com>
2022-10-06 13:29:45 +0300

    feat: created sign-up component, form-input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,40 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+// import { useEffect } from 'react'
+// import { getRedirectResult } from 'firebase/auth'
 
+import {
+  // auth,
+  signInWithGooglePopup,
+  // signInWithGoogleRedirect,
+  createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils'
+
+
+import SingUpForm from '../../components/sign-up-form/sing-up-form.component'
 
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await getRedirectResult(auth)
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
+
   return (
     <>
       <div>SignIn</div>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+      <SingUpForm />
     </>
   )
 }

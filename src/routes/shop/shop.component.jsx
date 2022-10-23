b55a15fd88e6a4c8f77c15dd6c71542d4459bb3e
HelenBassa <helenbassa@gmail.com>
2022-10-23 16:36:07 +0300

    feat: move categories into redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from "react-redux";
+
+import { getCategoriesAndDocument } from '../../utils/firebase/firebase.utils'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
 
+import { setCategoriesMap } from '../../store/categories/category.action'
+
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocument()
+      dispatch(setCategoriesMap(categoriesMap))
+    }
+    getCategoriesMap()
+    // eslint-disable-next-line
+  }, [])
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />

52bc8a61be7227f223dd254f4722aa9a62a52d41
HelenBassa <helenbassa@gmail.com>
2023-01-29 16:47:03 +0300

    feat: add Stripe payment

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux'
 
 import { selectCartTotal, selectCartItems } from '../../store/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
@@ -39,6 +40,7 @@ const Checkout = () => {
         )
       }
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   )
 }

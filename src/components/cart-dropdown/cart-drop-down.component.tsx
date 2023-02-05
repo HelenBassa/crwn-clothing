f14752d197b8be1677239d7c8003643ba4b56ec7
HelenBassa <helenbassa@gmail.com>
2023-02-05 11:58:31 +0300

    feat: optimize with hooks snd codesplitting

--- src/components/cart-dropdown/cart-drop-down.component.tsx
+++ src/components/cart-dropdown/cart-drop-down.component.tsx
@@ -1,39 +1,39 @@
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useCallback } from "react";
 
-import { selectCartItems } from '../../store/cart/cart.selector'
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import Button from '../button/button.component'
-import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from "../../store/cart/cart.selector";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
-  CartItems
-} from './cart-drop-down.styles'
+  CartItems,
+} from "./cart-drop-down.styles";
 
 const CartDropdown = () => {
-  const cartItems = useSelector(selectCartItems)
-  const navigate = useNavigate()
+  const cartItems = useSelector(selectCartItems);
+  const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout')
-  }
+  const goToCheckoutHandler = useCallback(() => {
+    navigate("/checkout");
+  }, []);
 
   return (
     <CartDropdownContainer>
       <CartItems>
-        {
-          cartItems.length ? (
-            cartItems.map((item) =>
-              <CartItem key={item.id} cartItem={item} />)
-          ) : (
-            <EmptyMessage>Your cart is empty</EmptyMessage>
-          )}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
       </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
-  )
-}
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
